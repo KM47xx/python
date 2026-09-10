402b54a0d2b159c5f0ab9ec8ecff98d956727ff5
KM47xx <krnmnjre9@gmail.com>
2026-09-10 18:52:40 +0530

    hw

--- pythonQ.py
+++ pythonQ.py
@@ -9,3 +9,11 @@ print(min//60," hours ",min%60," minutes")
 
 num=int(input())
 print(num," last digit:",abs(num%10))
+
+#inputs: role(student/teacher),age.   eligible if role==student and age<=21
+role=input("Enter role(Teacher/Student):")
+age=int(input("Enter age:"))
+if(age<=21 and role=="Student"):
+    print("eligible")
+else:
+    print("not elgible")
