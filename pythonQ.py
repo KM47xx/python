0b74d366a25e88787acc847fbcd81d718899f4a2
KM47xx <krnmnjre9@gmail.com>
2026-09-10 15:15:34 +0530

    q3

--- pythonQ.py
+++ pythonQ.py
@@ -1,8 +1,8 @@
-# x=int(input())
-# print("Number is odd:",x%2!=0)
+x=int(input())
+print("Number is odd:",x%2!=0)
 
-# age=int(input("Enter age:"))
-# print(age," years = ",age*365," days")
+age=int(input("Enter age:"))
+print(age," years = ",age*365," days")
 
 min = int(input("Enter minutes:"))
 print(min//60," hours ",min%60," minutes")
