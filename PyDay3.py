c906c8856b4d229f0e519ffede311f321c69dd59
KM47xx <krnmnjre9@gmail.com>
2026-09-15 10:10:34 +0530

    break

--- PyDay3.py
+++ PyDay3.py
@@ -9,8 +9,17 @@
 #         notfound=False
 # print("correct password")
 
+# n=int(input("Enter no:"))
+# i=0
+# while i<n:
+#     print(i)
+#     i+=1
+
 n=int(input("Enter no:"))
 i=0
+print("Even nums:")
 while i<n:
+    if(i%2!=0):
+        break
     print(i)
     i+=1
