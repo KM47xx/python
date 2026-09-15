214170189fa3c80eb2c7b8f819548bc50a98e01e
KM47xx <krnmnjre9@gmail.com>
2026-09-15 10:02:46 +0530

    while3

--- PyDay3.py
+++ PyDay3.py
@@ -1,9 +1,16 @@
 # while True:
 #     print("abc")
-correctPassword="abcd"
-notfound=True
-while(notfound):
-    passwordIn=input("Enter password:")
-    if(passwordIn==correctPassword):
-        notfound=False
-print("correct password")
+
+# correctPassword="abcd"
+# notfound=True
+# while(notfound):
+#     passwordIn=input("Enter password:")
+#     if(passwordIn==correctPassword):
+#         notfound=False
+# print("correct password")
+
+n=int(input("Enter no:"))
+i=0
+while i<n:
+    print(i)
+    i+=1
