650ca9ba0aeeceb1efec88a0a96695b2c765e8f4
KM47xx <krnmnjre9@gmail.com>
2026-09-15 10:32:03 +0530

    for

---
+++ while3.py
@@ -0,0 +1,5 @@
+n=int(input("Enter no:"))
+i=0
+while i<n:
+    print(i)
+    i+=1
---
+++ while4.py
@@ -0,0 +1,8 @@
+n=int(input("Enter no:"))
+i=0
+print("Even nums:")
+while i<n:
+    if(i%2!=0):
+        break
+    print(i)
+    i+=1
